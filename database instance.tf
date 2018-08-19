8b68e540caa08b7815138bf490875f9a1cb61973
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-19 07:42:06 +0530

    Create database instance.tf

---
+++ database instance.tf
@@ -0,0 +1,13 @@
+# Define database inside the private subnet
+resource "aws_instance" "db" {
+   ami  = "${var.ami}"
+   instance_type = "t1.micro"
+   key_name = "${aws_key_pair.default.id}"
+   subnet_id = "${aws_subnet.private-subnet.id}"
+   vpc_security_group_ids = ["${aws_security_group.sgdb.id}"]
+   source_dest_check = false
+
+  tags {
+    Name = "database"
+  }
+}
