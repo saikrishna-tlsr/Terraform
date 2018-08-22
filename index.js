18c0c87ba88d40feb8274814697db670cae3c78e
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 17:55:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ exports.handler = function(event, context, callback) {
     console.log('Event: ', JSON.stringify(event, null, '\t'));
     console.log('Context: ', JSON.stringify(context, null, '\t'));
     callback(null);
-    zip -r function.zip index.js
 };
