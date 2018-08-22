ce99a2904e66ad6b55ca4e40768a0a00f17a0fce
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:11:06 +0530

    Update autoscalling.tf

--- autoscalling.tf
+++ autoscalling.tf
@@ -16,7 +16,6 @@ resource "aws_launch_configuration" "example" {
 ## Creating AutoScaling Group
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
   min_size = 2
   max_size = 10
   load_balancers = ["${aws_elb.example.name}"]
