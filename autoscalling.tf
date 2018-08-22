ba53f02a82c00fb7a9cce47674e00b57d1e19a7f
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 12:48:41 +0530

    Create autoscalling.tf

---
+++ autoscalling.tf
@@ -0,0 +1,45 @@
+## Creating Launch Configuration
+resource "aws_launch_configuration" "example" {
+  image_id               = "${lookup(var.amis,var.region)}"
+  instance_type          = "t2.micro"
+  security_groups        = ["${aws_security_group.instance.id}"]
+  key_name               = "sai"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+## Creating AutoScaling Group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 10
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
