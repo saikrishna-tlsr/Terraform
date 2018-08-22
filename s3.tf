8fd0e84960f18fc86be2937bb565922dac4543e5
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 16:45:48 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,4 +6,13 @@ resource "aws_s3_bucket" "b" {
     Name        = "terraform bucket-tlsr"
     Environment = "Dev"
   }
-}
+
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.state_s3_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  }
