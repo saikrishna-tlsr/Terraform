efc6bf2feed1da1e66c5124a3e9073299ce1c1ab
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:19:11 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -2,7 +2,7 @@
 resource "aws_elb" "example" {
   name = "terraform-asg-example"
   security_groups = ["${aws_security_group.elb.id}"]
-  subnet_id = "${aws_subnet.public-subnet.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
