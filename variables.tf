8d41de0c69b00b553fee5cfeb0e5d7c168db370a
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:19:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "Region for the VPC"
   default = "us-east-1"
+  availability_zones = ["{data.aws_availability_zones.all.names}"]
 }
 variable "count" {
  default = 1
