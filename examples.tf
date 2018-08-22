152a95667aeeef519b4a1707a59d57464213b252
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:01:53 +0530

    Update examples.tf

--- examples.tf
+++ examples.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = “aws_access_key_id”
-  secret_key = “aws_secret_access_key_id”
+  access_key = "aws_access_key_id"
+  secret_key = "aws_secret_access_key_id"
   region     = "us-east-1"
 }
 data "aws_availability_zones" "all" {}
