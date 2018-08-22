271f23c6ccd6c6e2febb835539f7b9f763b03e08
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:34:32 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -2,7 +2,7 @@
 resource "aws_elb" "example" {
   name = "terraform-asg-example"
   security_groups = ["${aws_security_group.elb.id}"]
-  availability_zones = "${var.availability_zones}"
+  availability_zones = ["us-east-1a", "us-east-1b"]
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
