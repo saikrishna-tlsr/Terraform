3a34690e934964243da214b444b8234ce0ccfdb4
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 13:34:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "count" {
  default = 1
  }
 variable "availability_zones" {
-        default = "us-east-1a,us-east-1b"
+        default = ["us-east-1a", "us-east-1b"]
   }
 
 variable "vpc_cidr" {
