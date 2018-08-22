1e4c6ba0aa4b5a60158ca388b9dd0cca18712154
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 07:34:16 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "terraform-bucket"
+  bucket = "terraform-bucket-tlsr"
   acl    = "private"
 
   tags {
-    Name        = "terraform bucket"
+    Name        = "terraform bucket-tlsr"
     Environment = "Dev"
   }
 }
