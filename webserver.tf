e47f2586b950d4e0fa53cb7da4a377d9a35e063d
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 12:34:35 +0530

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -1,7 +1,7 @@
 # Define webserver inside the public subnet
 resource "aws_instance" "wb" {
    ami  = "${var.ami}"
-   count = “${var.count}”
+   count = "${var.count}"
    instance_type = "t1.micro"
    key_name = "sai"
    subnet_id = "${aws_subnet.public-subnet.id}"
@@ -11,6 +11,6 @@ resource "aws_instance" "wb" {
    user_data = "${file("install.sh")}"
 
   tags {
-    Name = "“${format(“wb-%03d”, count.index + 1)}”"
+    Name = "${format(“wb-%03d”, count.index + 1)}"
   }
 }
