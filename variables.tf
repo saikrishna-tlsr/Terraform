aa0232e948efd9a98b70b3f3527aca434afae840
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-22 12:28:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,13 @@ variable "aws_region" {
   description = "Region for the VPC"
   default = "us-east-1"
 }
+variable “count” {
+ default = 1
+ }
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
+  
   default = "10.0.0.0/16"
 }
 
