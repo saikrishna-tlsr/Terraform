c3e52b463c4218ca382ea518db73ca42f683fa60
Saikrishna <42440646+saikrishna-tlsr@users.noreply.github.com>
2018-08-23 12:09:54 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -e
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+
+project_path="${SCRIPT_DIR}/../../projects/${PROJECT}"
+
+
+echo "==============================================="
+echo "Start Building"
+echo "created vpc"
+echo "================================================"
